54ae3ef93c364340787a46000a7c8705f4c4488b
Sammy Seetharam <90780132+sammyseetharam@users.noreply.github.com>
2026-06-29 21:23:18 -0500

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows, cols = len(board), len(board[0])
+
+        #within this function, we check out of bounds, edges, or being an X and then breakout
+        #else we're good, to replace 
+        def dfs(r, c):
+            if (r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] == 'X'):
+                return 
+
+            board[r][c] = "T"
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        for r in range(rows): 
+            dfs(r, 0)
+            dfs(r, cols - 1)
+    
+        for c in range(cols): 
+            dfs(0, c)
+            dfs(rows - 1, c)
+        
+        for r in range(rows): 
+            for c in range(cols): 
+                if(board[r][c] == 'O'): 
+                    board[r][c] = 'X'
+
+                elif(board[r][c] == 'T'): 
+                    board[r][c] = 'O'
+        
+
+    
